1042ab34ca823bc14327eac3feecdc8d3c5a330f
DavidKingV <qet135ui@hotmail.com>
2024-04-30 12:16:27 -0600

    V2
    FIXED

--- src/mysql-database.ts
+++ src/mysql-database.ts
@@ -2,10 +2,6 @@ import "dotenv/config"
 import { MysqlAdapter } from '@builderbot/database-mysql'
 import mysql from 'mysql2/promise'
 
-const MYSQL_DB_HOST = process.env.MYSQL_DB_HOST 
-const MYSQL_DB_USER = process.env.MYSQL_DB_USER
-const MYSQL_DB_PASSWORD = process.env.MYSQL_DB_PASSWORD
-const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME
 const MYSQL_DB_PORT = 3306
 
 export type IDatabase = typeof MysqlAdapter
