a54f20fca3b81178b56976c805ff0303cabe7195
DavidKingV <qet135ui@hotmail.com>
2024-04-30 15:44:02 -0600

    Update blacklist.flow.ts

--- flow/blacklist.flow.ts
+++ flow/blacklist.flow.ts
@@ -4,7 +4,7 @@ import { numberClean } from "../src/utils/presence";
 
 export const blackListFlow = addKeyword('mute')
     .addAction(async (ctx, { blacklist, flowDynamic }) => {
-        if (ctx.from === '5215532479739') {
+        if (ctx.from === `${process.env.ADMIN_NUMBER}`) {
             console.log(ctx.from)
             const toMute = numberClean(ctx.body) //Mute +34000000 message incoming
             const check = blacklist.checkIf(toMute)
