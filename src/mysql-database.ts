700f158d7503bcb6b0aebaa3aab35fb5f9920ae7
DavidKingV <qet135ui@hotmail.com>
2024-04-30 10:26:24 -0600

    fix db
    la base de datos se cambia

--- src/mysql-database.ts
+++ src/mysql-database.ts
@@ -1,10 +1,10 @@
 import { MysqlAdapter } from '@builderbot/database-mysql'
 import mysql from 'mysql2/promise'
 
-const MYSQL_DB_HOST = 'localhost'
-const MYSQL_DB_USER = 'root'
-const MYSQL_DB_PASSWORD = ''
-const MYSQL_DB_NAME = 'bot'
+const MYSQL_DB_HOST = 'db5015748462.hosting-data.io'
+const MYSQL_DB_USER = 'dbu3066118'
+const MYSQL_DB_PASSWORD = 'Botunif123@'
+const MYSQL_DB_NAME = 'dbs12849036'
 const MYSQL_DB_PORT = 3306
 
 export type IDatabase = typeof MysqlAdapter
