38b8a55abb163857b594817043818ed503b2dff5
DavidKingV <qet135ui@hotmail.com>
2024-04-30 15:11:26 -0600

    Update mysql-database.ts

--- src/mysql-database.ts
+++ src/mysql-database.ts
@@ -1,10 +1,11 @@
+import "dotenv/config" 
 import { MysqlAdapter } from '@builderbot/database-mysql'
 import mysql from 'mysql2/promise'
 
-const MYSQL_DB_HOST = 'localhost'
-const MYSQL_DB_USER = 'root'
-const MYSQL_DB_PASSWORD = ''
-const MYSQL_DB_NAME = 'bot'
+const MYSQL_DB_HOST = process.env.MYSQL_DB_HOST
+const MYSQL_DB_USER = process.env.MYSQL_DB_USER
+const MYSQL_DB_PASSWORD = process.env.MYSQL_DB_PASSWORD
+const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME
 const MYSQL_DB_PORT = 3306
 
 export type IDatabase = typeof MysqlAdapter
