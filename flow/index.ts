30af782d1fbefa3b154853f196eefca5e0a740f8
DavidKingV <qet135ui@hotmail.com>
2024-05-16 10:42:02 -0600

    Se quita el flow test

--- flow/index.ts
+++ flow/index.ts
@@ -5,9 +5,9 @@ import { appointmentFlow } from './appointment.flow';
 import { reappointmentFlow } from './reappointment.flow';
 import { cancelappointmentFlow } from './cancelappointment.flow';
 import { agentFlow } from './agent.flow';
-import { questionFlow } from './test.flow';
+/*import { questionFlow } from './test.flow';*/
 import { mediaFlow, documentFlow } from './media.flow';
 import { idleFlow } from '../src/utils/idle-custom';
 import { flowVoiceNote } from './voicenote.flow';
 
-export const flow = createFlow([welcomeFlow, blackListFlow, appointmentFlow, reappointmentFlow, cancelappointmentFlow, agentFlow, questionFlow, mediaFlow, documentFlow,idleFlow,flowVoiceNote]);
+export const flow = createFlow([welcomeFlow, blackListFlow, appointmentFlow, reappointmentFlow, cancelappointmentFlow, agentFlow, mediaFlow, documentFlow,idleFlow,flowVoiceNote]);
